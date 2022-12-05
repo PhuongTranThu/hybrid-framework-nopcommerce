828e499aa7d679e58fa739d5f01e3e48f84a77d2
Phuong <phuongtranbka@gmail.com>
2022-12-05 23:04:36 +0700

    BrowserName

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,39 @@
 package commons;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
+	private WebDriver driver;
+
+	protected WebDriver getBrowserDriver(String browserName) {
+		BrowserName browser = BrowserName.valueOf(browserName.toUpperCase());
+
+		switch (browser) {
+		case FIREFOX:
+			driver = WebDriverManager.firefoxdriver().create();
+			break;
+
+		case CHROME:
+			driver = WebDriverManager.chromedriver().create();
+			break;
+
+		case EDGE:
+			driver = WebDriverManager.edgedriver().create();
+			break;
+
+		case OPERA:
+			driver = WebDriverManager.operadriver().create();
+			break;
+
+		default:
+			throw new RuntimeException("Browser is invalid");
+		}
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		return driver;
+	}
 
 }
---
+++ actions/commons/BrowserName.java
@@ -0,0 +1,6 @@
+package commons;
+
+public enum BrowserName {
+
+	CHROME, FIREFOX, EDGE, OPERA;
+}
