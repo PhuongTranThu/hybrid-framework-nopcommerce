515494c73c1e31908f0b3626ab44c858f90c82a1
Phuong <phuongtranbka@gmail.com>
2023-01-09 22:55:43 +0700

    Variable Property

---
+++ javaTester/javaOOP/Topic_02_Variable_Property_Method.java
@@ -0,0 +1,16 @@
+package javaOOP;
+
+public class Topic_02_Variable_Property_Method {
+
+	private String studentname = "AutomationFC";
+
+	public static String studentAddress = "Ha Noi";
+
+	final String country = "Viet Nam";
+	static final float PI_NUMBER = 3.14f;
+
+	public static void main(String[] args) {
+
+	}
+
+}
