a3b45d49c941ca8caa8c7e8c14f18842fe51f064
Phuong <phuongtranbka@gmail.com>
2023-07-06 23:14:55 +0700

    Managedata_Json

---
+++ testdata/com/nopcommerce/data/UserDataMapper.java
@@ -0,0 +1,68 @@
+package com.nopcommerce.data;
+
+import java.io.File;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.json.UTF8DataInputJsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import commons.GlobalConstants;
+
+public class UserDataMapper {
+
+	@JsonProperty("firstName")
+	private String firstName;
+	
+	@JsonProperty("lastName")
+	private String lastName;
+	
+	@JsonProperty("emailAddress")
+	private String emailAddress;
+	
+	@JsonProperty("validPassword")
+	private String validPassword;
+	
+	@JsonProperty("date")
+	private String date;
+	
+	@JsonProperty("month")
+	private String month;
+	
+	@JsonProperty("year")
+	private String year;
+	
+	public static UserDataMapper getUserData() {
+		try {
+			ObjectMapper mapper =  new ObjectMapper();
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+			return mapper.readValue(new File(GlobalConstants.PROJECT_PATH + "/resources/UserData.json"), UserDataMapper.class);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public String getValidPassword() {
+		return validPassword;
+	}
+	public String getDate() {
+		return date;
+	}
+	public String getMonth() {
+		return month;
+	}
+	public String getYear() {
+		return year;
+	}
+
+}
